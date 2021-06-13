a12bb84406d9092186698896a667052a1ac7a800
Somali <63553140+Somali19@users.noreply.github.com>
2021-06-13 21:18:00 +0530

    Update and rename index.html to home.php

--- home.php
+++ home.php
@@ -24,7 +24,7 @@
     <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto  ">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+            <a class="nav-link active" aria-current="page" href="home.php">Home</a>
           </li>
           
           <li class="nav-item">
